3e133d10dd8a892044980b2c08874468123bef7b
SeokHo Kim <kim76744@naver.com>
2021-08-05 23:05:43 +0900

    style : 세미콜론 추가

--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -70,7 +70,7 @@ public class Order {
      */
     public void cancel() {
         if (delivery.getStatus() == DeliveryStatus.COMP) {
-            throw new IllegalStateException("이미 배송 완료된 상품은 취소가 불가능합니다.")
+            throw new IllegalStateException("이미 배송 완료된 상품은 취소가 불가능합니다.");
         }
 
         this.setStatus(OrderStatus.CANCEL);
