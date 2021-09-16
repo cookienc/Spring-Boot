982c2ef90190435bd4ca3d3d514e31949c615fd7
SeokHo Kim <kim76744@naver.com>
2021-09-16 17:56:02 +0900

    fix : MemberApiController
    saveMemberV2가 안되는 문제해결

--- jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -25,27 +25,22 @@ public class MemberApiController {
 
     @PostMapping("/api/v2/members")
     public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
-
         Member member = new Member();
         member.setName(request.getName());
-
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
-
     }
 
     @Data
-    private class CreateMemberResponse {
+    static class CreateMemberResponse {
         private Long id;
-
         public CreateMemberResponse(Long id) {
             this.id = id;
         }
     }
 
     @Data
-    private class CreateMemberRequest {
-        @NotEmpty
+    static class CreateMemberRequest {
         private String name;
     }
 }
