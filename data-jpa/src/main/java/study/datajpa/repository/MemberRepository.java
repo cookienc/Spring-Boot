c2d4b8fee5becbc32657688dbb7f7a8950ec7a1d
SeokHo Kim <kim76744@naver.com>
2021-09-28 15:17:50 +0900

    feat : 벌크성 수정 쿼리(JPA&DataJPA)
    단체로 업데이트 하는 것을 벌크성 수정이라고 함

--- data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -3,6 +3,7 @@ package study.datajpa.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import study.datajpa.dto.MemberDto;
@@ -19,7 +20,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     List<Member> findByUsername(@Param("username") String username);
 
     @Query("select m from Member m where m.username = :username and m.age = :age")
-    List<Member> findUser(@Param("username") String username,@Param("age") int age);
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
 
     @Query("select m.username from Member m")
     List<String> findUsernameList();
@@ -31,10 +32,16 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     List<Member> findByNames(@Param("names") List<String> names);
 
     List<Member> findListByUsername(String username); //컬렉션
+
     Member findMemberByUsername(String username); // 단건
+
     Optional<Member> findOptionalByUsername(String username); //단건 optional
 
-//    @Query(value = "select m from Member m left join m.team t",
+    //    @Query(value = "select m from Member m left join m.team t",
 //            countQuery = "select count(m) from Member m")
     Page<Member> findByAge(int age, Pageable pageable);
+
+    @Modifying(clearAutomatically = true)
+    @Query("update Member m set m.age = m.age + 1 where m.age >= :age")
+    int bulkAgePlus(@Param("age") int age);
 }
