ed3beb9a4e7414dfb37e44a77f09420eba70598b
SeokHo Kim <kim76744@naver.com>
2021-10-01 15:25:59 +0900

    feat : where절로 동적쿼리 해결

--- querydsl/src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
+++ querydsl/src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
@@ -53,7 +53,7 @@ class MemberJpaRepositoryTest {
         condition.setAgeLoe(45);
         condition.setTeamName("teamB");
 
-        List<MemberTeamDto> result = memberJpaRepository.searchByBuilder(condition);
+        List<MemberTeamDto> result = memberJpaRepository.search(condition);
 
         assertThat(result).extracting("username").containsExactly("member4");
     }
